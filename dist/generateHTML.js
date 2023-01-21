31bc81ccb5b54da7363747399b8712a074043a14
Analise <analise.giobbi@gc.com>
2023-01-21 15:36:24 -0500

    inquirer setup and html base

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -1,4 +1,19 @@
 function generateHTML(){
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>Employee Website</title>
+
+        <head>
+        <body>
+            <header>
+            <header>
+        <body>
+    `
 
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHTML = require('./dist/generateHTML');
 
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name: ',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee ID: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your employee email: ',
+    },
+    {
+        type: 'input',
+        name: 'office number',
+        message: 'Enter your office number: ',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter your Github username: ',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter your Github username: ',
+    },
+    {
+        type: 'input',
+        name: 'School',
+        message: 'Enter your school: ',
+    },
+    {
+        type: 'list',
+        name: 'School',
+        message: 'Add another team member, or finsh creating your team: ',
+        choices: ['Engineer', 'Intern', 'Finish creating your team'],
+    },
+]
